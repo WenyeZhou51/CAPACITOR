3c62c0571fbe833bb89a9d34a5d825d6db44e3b5
Wenye Zhou <wz51@rice.edu>
2025-03-31 15:55:26 -0500

    made changes

--- Scenes/gameover.gd
+++ Scenes/gameover.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_button_pressed() -> void:
-	click_good.play()
+	#click_good.play()
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
